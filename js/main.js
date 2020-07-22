7962d078a714955961de970d6e35f8dd72cbefe4
feldmeow <feldmeow@gmail.com>
2020-07-23 02:22:30 +0300

    create: add review section & fix hotel slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev"
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev"
   },
   keyboard: {
     enabled: true,
@@ -13,28 +13,17 @@ var mySwiper = new Swiper(".swiper-container", {
   effect: "coverflow"
 });
 
-ymaps.ready(init);
-function init() {
-  // Создание карты.
-  var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [7.891189, 98.294772],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 15
-  });
-
-  var myPlacemark = new ymaps.Placemark(
-    [7.891189, 98.294772],
-    {},
-    {
-      preset: "islands#redIcon",
-      iconCaption: "Очень длиннный, но невероятно интересный текст"
-    }
-  );
-
-  myMap.geoObjects.add(myPlacemark);
-}
+var reviewSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev"
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true
+  },
+  effect: "slide"
+});
