2cd387df524a3dabc9b06bd0e57657e1898988f0
feldmeow <feldmeow@gmail.com>
2020-07-22 15:41:18 +0300

    create: media rules for hotelsection & add map api

--- js/main.js
+++ js/main.js
@@ -12,3 +12,29 @@ var mySwiper = new Swiper(".swiper-container", {
   },
   effect: "coverflow"
 });
+
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [7.573638, 79.803867],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 11
+  });
+
+  var myPlacemark = new ymaps.Placemark(
+    [7.573638, 79.803867],
+    {},
+    {
+      preset: "islands#redIcon",
+      iconCaption: "Очень длиннный, но невероятно интересный текст"
+    }
+  );
+
+  myMap.geoObjects.add(myPlacemark);
+}
