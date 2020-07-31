b648a07a9f2d1eb08960ac0efb8b0e164ba16e99
feldmeow <feldmeow@gmail.com>
2020-08-01 00:21:56 +0300

    create: mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
 var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev"
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true
-  },
-  effect: "coverflow"
+	// Optional parameters
+	loop: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: ".hotel-slider__button--next",
+		prevEl: ".hotel-slider__button--prev",
+	},
+	keyboard: {
+		enabled: true,
+		onlyInViewport: true,
+	},
+	effect: "coverflow",
 });
 
 var reviewSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev"
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true
-  },
-  effect: "slide"
+	// Optional parameters
+	loop: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: ".reviews-slider__button--next",
+		prevEl: ".reviews-slider__button--prev",
+	},
+	keyboard: {
+		enabled: true,
+		onlyInViewport: true,
+	},
+	effect: "slide",
+});
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+	document
+		.querySelector(".navbar-bottom")
+		.classList.toggle("navbar-bottom_visible");
 });
